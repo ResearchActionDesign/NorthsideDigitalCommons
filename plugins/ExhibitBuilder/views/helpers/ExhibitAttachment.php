2dc189ef5fa2145d3bc7d6120fc49ae1889f1995
Tim Stallmann <tim@stallmann.mayfirst.org>
2016-04-16 20:49:15 -0400

    Add html5 player to exhibit page galleries

--- plugins/ExhibitBuilder/views/helpers/ExhibitAttachment.php
+++ plugins/ExhibitBuilder/views/helpers/ExhibitAttachment.php
@@ -21,23 +21,59 @@ class ExhibitBuilder_View_Helper_ExhibitAttachment extends Zend_View_Helper_Abst
     {
         $item = $attachment->getItem();
         $file = $attachment->getFile();
-        
-        if ($file) {
-            if (!isset($fileOptions['imgAttributes']['alt'])) {
-                $fileOptions['imgAttributes']['alt'] = metadata($item, array('Dublin Core', 'Title'));
+        static $html5_audio_types;
+        static $html5_video_types;
+        static $size_constraint;
+
+        if (class_exists('Html5MediaPlugin')) {
+            if (!isset($html5_audio_types) || !isset($html5_video_types)) {
+                $html5_settings = unserialize(get_option('html5_media_settings'));
+                $html5_audio_types = $html5_settings['audio']['types'];
+                $html5_video_types = $html5_settings['video']['types'];
             }
-            
-            if ($forceImage) {
-                $imageSize = isset($fileOptions['imageSize'])
-                    ? $fileOptions['imageSize']
-                    : 'square_thumbnail';
-                $image = file_image($imageSize, $fileOptions['imgAttributes'], $file);
-                $html = exhibit_builder_link_to_exhibit_item($image, $linkProps, $item);
-            } else {
-                if (!isset($fileOptions['linkAttributes']['href'])) {
-                    $fileOptions['linkAttributes']['href'] = exhibit_builder_exhibit_item_uri($item);
+
+            if (!isset($size_constraint)) {
+                $size_constraint = get_option('square_thumbnail_constraint');
+            }
+        }
+
+        if ($file) {
+            // HTML 5 media hook
+            if (class_exists('Html5MediaPlugin')) {
+                if (in_array($file->mime_type, $html5_audio_types)) {
+                    $html = Html5MediaPlugin::audio($file, array(
+                      'height' => $size_constraint,
+                      'width' => $size_constraint
+                    ));
                 }
-                $html = file_markup($file, $fileOptions, null);
+                elseif (in_array($file->mime_type, $html5_video_types)) {
+                    $html = Html5MediaPlugin::video($file, array(
+                      'height' => $size_constraint,
+                      'width' => $size_constraint
+                    ));
+                }
+            }
+            if (!isset($html)) {
+                if (!isset($fileOptions['imgAttributes']['alt'])) {
+                    $fileOptions['imgAttributes']['alt'] = metadata($item, array(
+                      'Dublin Core',
+                      'Title'
+                    ));
+                }
+
+                if ($forceImage) {
+                    $imageSize = isset($fileOptions['imageSize'])
+                      ? $fileOptions['imageSize']
+                      : 'square_thumbnail';
+                    $image = file_image($imageSize, $fileOptions['imgAttributes'], $file);
+                    $html = exhibit_builder_link_to_exhibit_item($image, $linkProps, $item);
+                }
+                else {
+                    if (!isset($fileOptions['linkAttributes']['href'])) {
+                        $fileOptions['linkAttributes']['href'] = exhibit_builder_exhibit_item_uri($item);
+                    }
+                    $html = file_markup($file, $fileOptions, NULL);
+                }
             }
         } else if($item) {
             $html = exhibit_builder_link_to_exhibit_item(null, $linkProps, $item);
