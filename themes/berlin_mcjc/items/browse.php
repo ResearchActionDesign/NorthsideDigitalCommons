2d107146d0a8f322dfaee192b0796bbfb78abd08
Tim Stallmann <tim@stallmann.mayfirst.org>
2017-01-10 17:21:32 -0500

    Retheme browse items page for browse people view

--- themes/berlin_mcjc/items/browse.php
+++ themes/berlin_mcjc/items/browse.php
@@ -1,21 +1,28 @@
 <?php
-$pageTitle = __('Browse Items');
+// Display separate theming if this is specifically a browse people page.
+$filters = item_search_filters();
+if (strpos($filters, 'Item Type: Person') !== FALSE) {
+    $browseByPerson = TRUE;
+    $pageTitle = __('People');
+}
+else {
+    $browseByPerson = FALSE;
+    $pageTitle = __('Browse Items');
+}
 echo head(array('title'=>$pageTitle,'bodyclass' => 'items browse'));
 ?>
 
 <h1><?php echo $pageTitle;?> <?php echo __('(%s total)', $total_results); ?></h1>
 
+<?php if (!$browseByPerson): ?>
 <nav class="items-nav navigation secondary-nav">
     <?php echo public_nav_items(); ?>
 </nav>
-
-<?php echo item_search_filters(); ?>
-
-<?php echo pagination_links(); ?>
-
-<?php if ($total_results > 0): ?>
-
 <?php
+endif;
+echo $browseByPerson ? '' : $filters;
+echo pagination_links();
+if (($total_results > 0) && (!$browseByPerson)):
 $sortLinks[__('Title')] = 'Dublin Core,Title';
 $sortLinks[__('Creator')] = 'Dublin Core,Creator';
 $sortLinks[__('Date Added')] = 'added';
@@ -23,7 +30,6 @@ $sortLinks[__('Date Added')] = 'added';
 <div id="sort-links">
     <span class="sort-label"><?php echo __('Sort by: '); ?></span><?php echo browse_sort_links($sortLinks); ?>
 </div>
-
 <?php endif; ?>
 
 <?php foreach (loop('items') as $item): ?>
@@ -35,7 +41,12 @@ $sortLinks[__('Date Added')] = 'added';
     </div>
     <?php endif; ?>
 	<div class="citation">
-	 <?php echo link_to_item('<h3>' . metadata('item', array('Dublin Core', 'Title')) . ' (' . metadata('item', array('Dublin Core', 'Type')) . ')</h3>'); ?>
+	 <?php
+   $item_type = metadata('item', array('Dublin Core', 'Type'));
+   if ($item_type <> '') {
+       $item_type = ' (' . $item_type . ')';
+   }
+   echo link_to_item('<h3>' . metadata('item', array('Dublin Core', 'Title')) . $item_type . '</h3>'); ?>
 	 </div>
     <?php if ($description = metadata('item', array('Dublin Core', 'Description'), array('snippet'=>325))): ?>
     <div class="item-description">
@@ -52,7 +63,7 @@ $sortLinks[__('Date Added')] = 'added';
     <?php fire_plugin_hook('public_items_browse_each', array('view' => $this, 'item' =>$item)); ?>
 
     </div><!-- end class="item-meta" -->
-</div><!-- end class="item hentry" -->
+</div><!-- end class="item entry" -->
 <?php endforeach; ?>
 
 <?php echo pagination_links(); ?>
