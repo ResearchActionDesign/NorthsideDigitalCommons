57584ca5926b19cc20b85b74176dbb95200c17a6
Tim Stallmann <tim@stallmann.mayfirst.org>
2017-02-07 11:02:29 -0500

    Add details expander on item show metadata

--- htdocs/themes/berlin_mcjc/items/show.php
+++ htdocs/themes/berlin_mcjc/items/show.php
@@ -2,53 +2,50 @@
 queue_js_file('lightbox.min', 'javascripts/vendor');
 queue_css_file('lightbox');
 ?>
-<?php echo head(array('title' => metadata('item', array('Dublin Core', 'Title')),'bodyclass' => 'items show')); ?>
+<?php echo head(array('title' => metadata('item', array('Dublin Core', 'Title')),'bodyclass' => 'item show')); ?>
+
 <div id="primary">
     <h1><?php echo metadata('item', array('Dublin Core','Title')); ?></h1>
   <div id="item-description">
     <?php echo metadata('item', array('Dublin Core','Description')); ?>
   </div>
 
-  <?php
-  // If item is a photograph, show the photograph first and then metadata.
-  $is_photo = FALSE;
-  if (metadata('item', array('Dublin Core', 'Type')) == 'Photograph'):
-    $is_photo = TRUE; ?>
-    <div class="item-photograph"><?php echo item_image_gallery(array('link'=>array('data-lightbox'=>'lightbox'))); ?></div>
+  <!-- Item files -->
+  <?php if (metadata('item', 'has files')): ?>
+    <div id="itemfiles" class="element">
+      <div class="element-text"><?php echo item_image_gallery(); ?></div>
+    </div>
   <?php endif; ?>
 
-    <!-- Items metadata -->
-    <div id="item-metadata">
-      <?php if ($is_photo === FALSE): ?>
-        <h3><?php echo __('Files'); ?></h3>
-        <div id="item-images">
-          <?php echo files_for_item(); ?>
-        </div>
-      <?php endif; ?>
-        <?php echo all_element_texts('item'); ?>
-    </div>
+  <!-- Other metadata -->
+  <details id="item-metadata">
+    <summary><h2><?php echo __('Details') ?></h2></summary>
+    <?php echo all_element_texts('item'); ?>
 
-   <?php if(metadata('item','Collection Name')): ?>
+    <?php if (metadata('item', 'Collection Name')): ?>
       <div id="collection" class="element">
         <h3><?php echo __('Collection'); ?></h3>
-        <div class="element-text"><?php echo link_to_collection_for_item(); ?></div>
+        <div class="element-text"><p><?php echo link_to_collection_for_item(); ?></p></div>
       </div>
-   <?php endif; ?>
-
-     <!-- The following prints a list of all tags associated with the item -->
-    <?php if (metadata('item','has tags')): ?>
-    <div id="item-tags" class="element">
-        <h3><?php echo __('Tags'); ?></h3>
-        <div class="element-text"><?php echo tag_string('item'); ?></div>
-    </div>
-    <?php endif;?>
+    <?php endif; ?>
 
     <!-- The following prints a citation for this item. -->
     <div id="item-citation" class="element">
-        <h3><?php echo __('Citation'); ?></h3>
-        <div class="element-text"><?php echo metadata('item','citation',array('no_escape'=>true)); ?></div>
+      <h3><?php echo __('Citation'); ?></h3>
+      <div class="element-text"><?php echo metadata('item','citation',array('no_escape'=>true)); ?></div>
+      </div>
+  </details>
+
+
+  <!-- The following prints a list of all tags associated with the item -->
+  <?php if (metadata('item', 'has tags')): ?>
+    <div id="item-tags" class="element">
+      <h3><?php echo __('Tags'); ?></h3>
+      <div class="element-text"><?php echo tag_string('item'); ?></div>
     </div>
-       <?php fire_plugin_hook('public_items_show', array('view' => $this, 'item' => $item)); ?>
+  <?php endif;?>
+
+  <?php fire_plugin_hook('public_items_show', array('view' => $this, 'item' => $item)); ?>
 
 
     <ul class="item-pagination navigation">
