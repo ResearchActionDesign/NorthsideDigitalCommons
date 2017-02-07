602034a394f8f2d09dd7708111c67be9437828c8
Tim Stallmann <tim@stallmann.mayfirst.org>
2017-02-07 09:32:01 -0500

    Theming collections browse page

--- htdocs/themes/berlin_mcjc/collections/browse.php
+++ htdocs/themes/berlin_mcjc/collections/browse.php
@@ -13,7 +13,6 @@ echo head(array('title'=>$pageTitle,'bodyclass' => 'collections browse'));
     <h2><?php echo link_to_collection(); ?></h2>
     <?php if (metadata('collection', array('Dublin Core', 'Description'))): ?>
     <div class="element">
-        <h3><?php echo __('Description'); ?></h3>
         <div class="element-text"><?php echo text_to_paragraphs(metadata('collection', array('Dublin Core', 'Description'), array('snippet'=>325))); ?></div>
     </div>
     <?php endif; ?>
