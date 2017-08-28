699748a0317e02fb2cf6335ddda673e93f7cd453
Anne Tomasevich <annetvich@gmail.com>
2017-08-28 18:14:52 -0400

    Add larger images and adjust spacing on item browse and show pages

--- htdocs/themes/berlin_mcjc/items/show.php
+++ htdocs/themes/berlin_mcjc/items/show.php
@@ -14,7 +14,7 @@
   <!-- Item files -->
   <?php if (metadata('item', 'has files')): ?>
     <div id="itemfiles" class="element">
-      <div class="item-images"><?php echo mcjc_files_for_item(); ?>
+      <div class="item-images"><?php echo mcjc_files_for_item(array('imageSize' => 'fullsize')); ?>
       </div>
     </div>
   <?php endif; ?>
