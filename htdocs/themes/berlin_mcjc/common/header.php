3c80daf78b3e112584b497ddd77c96f47a92e74e
Tim Stallmann <tim@stallmann.mayfirst.org>
2017-10-23 16:08:09 -0400

    Add favicon

--- htdocs/themes/berlin_mcjc/common/header.php
+++ htdocs/themes/berlin_mcjc/common/header.php
@@ -33,6 +33,21 @@
     <?php queue_js_file('berlin'); ?>
     <?php queue_js_file('globals'); ?>
     <?php echo head_js(); ?>
+	<link rel="apple-touch-icon" sizes="57x57" href="/themes/berlin_mcjc/images/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="/themes/berlin_mcjc/images/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="/themes/berlin_mcjc/images/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="/themes/berlin_mcjc/images/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="/themes/berlin_mcjc/images/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="/themes/berlin_mcjc/images/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="/themes/berlin_mcjc/images/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="/themes/berlin_mcjc/images/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="/themes/berlin_mcjc/images/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192"  href="/themes/berlin_mcjc/images/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/themes/berlin_mcjc/images/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="/themes/berlin_mcjc/images/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/themes/berlin_mcjc/images/favicon-16x16.png">
+	<link rel="manifest" href="/themes/berlin_mcjc/images/manifest.json">
+	<meta name="msapplication-TileImage" content="/themes/berlin_mcjc/images/ms-icon-144x144.png">
 </head>
  <?php echo body_tag(array('id' => @$bodyid, 'class' => @$bodyclass)); ?>
     <a href="#content" id="skipnav"><?php echo __('Skip to main content'); ?></a>
