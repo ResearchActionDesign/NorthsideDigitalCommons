11b63b27a909512c1bb1749e0e2438a852383604
Tim Stallmann <tim@rad.cat>
2020-01-13 12:24:58 -0500

    Issue #9: Undo mistaken change to berlin/index.php

--- htdocs/themes/berlin/index.php
+++ htdocs/themes/berlin/index.php
@@ -1,10 +1,28 @@
 <?php echo head(array('bodyid'=>'home', 'bodyclass' =>'two-col')); ?>
 <div id="primary">
     <?php if ($homepageText = get_theme_option('Homepage Text')): ?>
-    <div id="homepage-text"><?php echo $homepageText; ?></div>
+    <p><?php echo $homepageText; ?></p>
     <?php endif; ?>
-</div>
-<!-- end primary -->
+    <?php if (get_theme_option('Display Featured Item') == 1): ?>
+    <!-- Featured Item -->
+    <div id="featured-item" class="featured">
+        <h2><?php echo __('Featured Item'); ?></h2>
+        <?php echo random_featured_items(1); ?>
+    </div><!--end featured-item-->
+    <?php endif; ?>
+    <?php if (get_theme_option('Display Featured Collection')): ?>
+    <!-- Featured Collection -->
+    <div id="featured-collection" class="featured">
+        <h2><?php echo __('Featured Collection'); ?></h2>
+        <?php echo random_featured_collection(); ?>
+    </div><!-- end featured collection -->
+    <?php endif; ?>
+    <?php if ((get_theme_option('Display Featured Exhibit')) && function_exists('exhibit_builder_display_random_featured_exhibit')): ?>
+    <!-- Featured Exhibit -->
+    <?php echo exhibit_builder_display_random_featured_exhibit(); ?>
+    <?php endif; ?>
+
+</div><!-- end primary -->
 
 <div id="secondary">
     <?php
