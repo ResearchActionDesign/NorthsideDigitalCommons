00c66263699de6b126d92e348ecc360046dfac17
Tim Stallmann <tim@rad.cat>
2020-01-13 12:24:10 -0500

    Issue #9: Start to implement homepage design on back-end

--- htdocs/themes/berlin/index.php
+++ htdocs/themes/berlin/index.php
@@ -1,28 +1,10 @@
 <?php echo head(array('bodyid'=>'home', 'bodyclass' =>'two-col')); ?>
 <div id="primary">
     <?php if ($homepageText = get_theme_option('Homepage Text')): ?>
-    <p><?php echo $homepageText; ?></p>
+    <div id="homepage-text"><?php echo $homepageText; ?></div>
     <?php endif; ?>
-    <?php if (get_theme_option('Display Featured Item') == 1): ?>
-    <!-- Featured Item -->
-    <div id="featured-item" class="featured">
-        <h2><?php echo __('Featured Item'); ?></h2>
-        <?php echo random_featured_items(1); ?>
-    </div><!--end featured-item-->
-    <?php endif; ?>
-    <?php if (get_theme_option('Display Featured Collection')): ?>
-    <!-- Featured Collection -->
-    <div id="featured-collection" class="featured">
-        <h2><?php echo __('Featured Collection'); ?></h2>
-        <?php echo random_featured_collection(); ?>
-    </div><!-- end featured collection -->
-    <?php endif; ?>
-    <?php if ((get_theme_option('Display Featured Exhibit')) && function_exists('exhibit_builder_display_random_featured_exhibit')): ?>
-    <!-- Featured Exhibit -->
-    <?php echo exhibit_builder_display_random_featured_exhibit(); ?>
-    <?php endif; ?>
-
-</div><!-- end primary -->
+</div>
+<!-- end primary -->
 
 <div id="secondary">
     <?php
