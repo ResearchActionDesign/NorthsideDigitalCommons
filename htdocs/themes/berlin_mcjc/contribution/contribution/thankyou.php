9368a1dac849bbd9ce55964436e2baf1f73d0690
Tim Stallmann <tim@rad.cat>
2020-03-17 15:10:44 -0400

    Tweak HTML for add your story paths
    Tweak theming for contribution pages

--- htdocs/themes/berlin_mcjc/contribution/contribution/thankyou.php
+++ htdocs/themes/berlin_mcjc/contribution/contribution/thankyou.php
@@ -1,8 +1,13 @@
-<?php echo head(); ?>
+<?php echo head(array('bodyclass' => 'contribution thank-you')); ?>
+<div class="title">
+    <h1><?php echo __("Add Your Story"); ?></h1>
+</div>
 <div id="primary">
-	<h1><?php echo __("Thank you for contributing!"); ?></h1>
-	<p><?php echo __("Your contribution will show up in the archive once an administrator approves it. Meanwhile, feel free to %s or %s ." , contribution_link_to_contribute(__('make another contribution')), "<a href='" . url('items/browse') . "'>" . __('browse the archive') . "</a>"); ?>
+    <h2><?php echo __("Thank you for contributing!"); ?></h2>
+    <p><?php echo __("Your contribution will show up in the archive once an administrator approves it."); ?>
 	</p>
+    <a class="button" href="<?php echo contribution_contribute_url(); ?>"><?php echo __('Add another story'); ?></a>
+    <a class='button' href="<?php echo url(array(), 'peopleDefault'); ?>"><?php echo __('View more stories'); ?></a>
 	<?php if(get_option('contribution_open') && !current_user()): ?>
 	<p><?php echo __("If you would like to interact with the site further, you can use an account that is ready for you. Visit %s, and request a new password for the email you used", "<a href='" . url('users/forgot-password') . "'>" . __('this page') . "</a>"); ?>
 	<?php endif; ?>
