e5ff81a396d593e6fbba89256fca0abc797b47f0
Kevin Arevalo <arevalok91@gmail.com>
2020-03-24 13:28:09 -0400

    Added a parent container to People and a child div

--- htdocs/themes/berlin_mcjc/people/index/browse.php
+++ htdocs/themes/berlin_mcjc/people/index/browse.php
@@ -11,34 +11,25 @@ echo head(['title' => $pageTitle, 'bodyclass' => 'people browse']);
 <h1><?php echo $pageTitle; ?></h1>
 
 <div class="search-by-lastname">
-    <span><?php echo __('SEARCH BY LAST NAME'); ?></span>
-    <ul>
-        <li<?php echo !$curLetter
-            ? ' class="active"'
-            : ''; ?>><a href="<?php echo $this->url(
-    [],
-    'peopleDefault'
-); ?>">All</a></li>
-        <?php foreach (range('A', 'Z') as $letter): ?>
-        <?php if (in_array($letter, $validLetters)): ?>
-                <li<?php echo $curLetter == $letter
-                    ? ' class="active"'
-                    : ''; ?>>
-                <a href="<?php echo $this->url([], 'peopleDefault', [
-                    'firstLetter' => $letter,
-                ]); ?>"><?php echo $letter; ?></a>
-                </li>
+  <span><?php echo __('SEARCH BY LAST NAME'); ?></span>
+  <ul>
+    <li<?php echo (!$curLetter ? ' class="active"' : '');?>><a
+        href="<?php echo $this->url(array(), 'peopleDefault')?>">All</a></li>
+      <?php foreach (range('A', 'Z') as $letter): ?>
+      <?php if (in_array($letter, $validLetters)): ?>
+      <li<?php echo ($curLetter == $letter ? ' class="active"' : '');?>>
+        <a
+          href="<?php echo $this->url(array(), 'peopleDefault', array('firstLetter' => $letter)) ?>"><?php echo $letter ?></a>
+        </li>
         <?php else: ?>
-                <li class="disabled">
-                    <?php echo $letter; ?>
-                </li>
+        <li class="disabled">
+          <?php echo $letter ?>
+        </li>
         <?php endif; ?>
         <?php endforeach; ?>
-    </ul>
+  </ul>
 </div>
-
-<?php foreach (loop('items') as $item): ?>
-  <?php
+<?php foreach (loop('items') as $item): ?> <?php
   $itemTitle = metadata('item', 'display_title');
   $itemClasses = "";
   if (metadata('item', 'has files')) {
@@ -47,44 +38,65 @@ echo head(['title' => $pageTitle, 'bodyclass' => 'people browse']);
   if (metadata('item', ['Dublin Core', 'Description'])) {
       $itemClasses .= " has-bio";
   }
-  ?>
-    <div class="item record<?php echo $itemClasses; ?>">
-        <h2><?php echo mcjc_link_to_item($itemTitle, $item); ?></h2>
-        <div class="item-meta">
-          <?php if (metadata('item', 'has files')): ?>
-              <div class="item-img">
-                  <?php echo item_image('square_thumbnail', [
+  ?> <div class="item record<?php echo $itemClasses; ?>">
+  <h2><?php echo mcjc_link_to_item($itemTitle, $item); ?></h2>
+  <div class="item-meta">
+    <?php if (metadata('item', 'has files')): ?>
+    <div class="item-img">
+      <?php echo item_image('square_thumbnail', [
                       'alt' => $itemTitle,
                   ]); ?>
-              </div>
-          <?php endif; ?>
-
-          <?php if (
+    </div>
+    <?php endif; ?>
+    <?php if (
               $description = metadata(
                   'item',
                   ['Dublin Core', 'Description'],
                   ['snippet' => 250]
               )
           ): ?>
-              <div class="item-description">
-                <?php echo $description; ?>
-              </div>
-          <?php endif; ?>
+    <div class="item-description">
+      <?php echo $description; ?>
+    </div>
+    <?php endif; ?>
 
-          <?php fire_plugin_hook('public_items_browse_each', [
+    <?php fire_plugin_hook('public_items_browse_each', [
               'view' => $this,
               'item' => $item,
           ]); ?>
 
-        </div><!-- end class="item-meta" -->
-    </div><!-- end class="item entry" -->
+  </div><!-- end class="item-meta" -->
+</div><!-- end class="item entry" -->
 <?php endforeach; ?>
+<div class="peoples-item-container">
+  <?php foreach (loop('items') as $item): ?> <?php
+    $itemTitle = metadata('item', 'display_title');
+    $itemClasses = "";
+    if (metadata('item', 'has files')) $itemClasses = " has-picture";
+    if (metadata('item', array('Dublin Core', 'Description'))) $itemClasses .= " has-bio";?>
+  <div class="person-cards">
+    <div class="item record<?php echo $itemClasses ?>">
+      <h2><?php echo people_get_link_to_item($itemTitle, array('class'=>'permalink')); ?></h2>
+      <div class="item-meta">
+        <?php if (metadata('item', 'has files')): ?>
+        <div class="item-img">
+          <?php echo item_image('square_thumbnail', array('alt' => $itemTitle)); ?>
+        </div>
+        <?php endif; ?>
 
-<?php echo pagination_links(); ?>
-
-<?php fire_plugin_hook('public_items_browse', [
+        <?php if ($description = metadata('item', array('Dublin Core', 'Description'), array('snippet'=>250))): ?>
+        <div class="item-description">
+          <?php echo $description; ?>
+        </div>
+        <?php endif; ?><?php fire_plugin_hook('public_items_browse_each', array('view' => $this, 'item' =>$item)); ?>
+        <?php fire_plugin_hook('public_items_browse', [
     'items' => $items,
     'view' => $this,
-]); ?>
-
-<?php echo foot(); ?>
+]); ?></div>
+      <!-- end class="item-meta" -->
+    </div><!-- end class="item entry" -->
+  </div>
+  <?php endforeach; ?>
+</div>
+<div <?php echo pagination_links(); ?>
+  <?php fire_plugin_hook('public_items_browse', array('items'=> $items, 'view' => $this)); ?> <?php echo foot(); ?>
