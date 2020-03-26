689589470257810f40fe59cdf1eb404f002bf590
Kevin Arevalo <arevalok91@gmail.com>
2020-03-26 10:39:53 -0400

    people cards have full width img and desscription

--- htdocs/themes/berlin_mcjc/people/index/browse.php
+++ htdocs/themes/berlin_mcjc/people/index/browse.php
@@ -1,9 +1,15 @@
 <?php
 
+
+
+
+
 $browseByPerson = TRUE;
 $pageTitle = __('Meet our Neighbors');
 $curLetter = $vars['cur_letter'];
 $validLetters = $vars['letters'];
+
+
 
 echo head(array('title'=>$pageTitle,'bodyclass' => 'people browse'));
 ?>
@@ -31,34 +37,37 @@ echo head(array('title'=>$pageTitle,'bodyclass' => 'people browse'));
 </div>
 
 <div class="peoples-item-container">
+
+
   <?php foreach (loop('items') as $item): ?> <?php
     $itemTitle = metadata('item', 'display_title');
     $itemClasses = "";
     if (metadata('item', 'has files')) $itemClasses = " has-picture";
-    if (metadata('item', array('Dublin Core', 'Description'))) $itemClasses .= " has-bio";?>
-  <div class="person-cards">
-    <div class="item record<?php echo $itemClasses ?>">
-      <h2><?php echo people_get_link_to_item($itemTitle, array('class'=>'permalink')); ?></h2>
-      <div class="item-meta">
-        <?php if (metadata('item', 'has files')): ?>
-        <div class="item-img">
-          <?php echo item_image('square_thumbnail', array('alt' => $itemTitle)); ?>
-        </div>
-        <?php endif; ?>
+    if (metadata('item', array('Dublin Core', 'Description'))) $itemClasses .= " has-bio";
+?>
+  <div class="item record<?php echo $itemClasses ?>">
+    <div class="item-img">
+      <?php echo item_image('square_thumbnail', array('alt' => $itemTitle)); ?>
+    </div>
+    <div class="item-meta">
+      <div>
+        <h2><?php echo people_get_link_to_item($itemTitle, array('class'=>'permalink')); ?></h2>
+      </div>
+      <?php if (metadata('item', 'has files')): ?>
+      <?php endif; ?>
 
-        <?php if ($description = metadata('item', array('Dublin Core', 'Description'), array('snippet'=>250))): ?>
-        <div class="item-description">
-          <?php echo $description; ?>
-        </div>
-        <?php endif; ?>
+      <?php if ($description = metadata('item', array('Dublin Core', 'Description'), array('snippet'=>250))): ?>
+      <div class="item-description">
+        <?php echo $description; ?>
+      </div>
+      <?php endif; ?>
 
-        <?php fire_plugin_hook('public_items_browse_each', array('view' => $this, 'item' =>$item)); ?>
+      <?php fire_plugin_hook('public_items_browse_each', array('view' => $this, 'item' =>$item)); ?>
 
-      </div><!-- end class="item-meta" -->
-    </div><!-- end class="item entry" -->
-  </div>
+    </div><!-- end class="item-meta" -->
+  </div><!-- end class="item entry" -->
+
   <?php endforeach; ?>
-
 </div>
 <div <?php echo pagination_links(); ?>
   <?php fire_plugin_hook('public_items_browse', array('items'=> $items, 'view' => $this)); ?> <?php echo foot(); ?>
