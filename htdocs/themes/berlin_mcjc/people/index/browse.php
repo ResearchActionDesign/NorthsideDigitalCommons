666003f36c3d840f0ee6fedf452e8c24b2df0a92
Kevin Arevalo <arevalok91@gmail.com>
2020-04-08 17:33:35 -0400

    rebase finished commit

--- htdocs/themes/berlin_mcjc/people/index/browse.php
+++ htdocs/themes/berlin_mcjc/people/index/browse.php
@@ -9,11 +9,13 @@ $validLetters = $vars['letters'];
 echo head(array('title'=>$pageTitle,'bodyclass' => 'people browse'));
 ?>
 <div class="top-header">
+
   <h1><?php echo $pageTitle;?></h1>
   <p>
     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer suscipit diam a nulla tempus rhoncus. Aliquam erat
     volutpat.
   </p>
+
 </div>
 <div class="search-by-lastname">
   <span><?php echo __('SEARCH BY LAST NAME'); ?></span>
@@ -34,45 +36,7 @@ echo head(array('title'=>$pageTitle,'bodyclass' => 'people browse'));
         <?php endforeach; ?>
   </ul>
 </div>
-<?php foreach (loop('items') as $item): ?> <?php
-  $itemTitle = metadata('item', 'display_title');
-  $itemClasses = "";
-  if (metadata('item', 'has files')) {
-      $itemClasses = " has-picture";
-  }
-  if (metadata('item', ['Dublin Core', 'Description'])) {
-      $itemClasses .= " has-bio";
-  }
-  ?> <div class="item record<?php echo $itemClasses; ?>">
-  <h2><?php echo mcjc_link_to_item($itemTitle, $item); ?></h2>
-  <div class="item-meta">
-    <?php if (metadata('item', 'has files')): ?>
-    <div class="item-img">
-      <?php echo item_image('square_thumbnail', [
-                      'alt' => $itemTitle,
-                  ]); ?>
-    </div>
-    <?php endif; ?>
-    <?php if (
-              $description = metadata(
-                  'item',
-                  ['Dublin Core', 'Description'],
-                  ['snippet' => 250]
-              )
-          ): ?>
-    <div class="item-description">
-      <?php echo $description; ?>
-    </div>
-    <?php endif; ?>
 
-    <?php fire_plugin_hook('public_items_browse_each', [
-              'view' => $this,
-              'item' => $item,
-          ]); ?>
-
-  </div><!-- end class="item-meta" -->
-</div><!-- end class="item entry" -->
-<?php endforeach; ?>
 <div class="peoples-item-container">
 
 
@@ -81,29 +45,33 @@ echo head(array('title'=>$pageTitle,'bodyclass' => 'people browse'));
     $itemClasses = "";
     if (metadata('item', 'has files')) $itemClasses = " has-picture";
     if (metadata('item', array('Dublin Core', 'Description'))) $itemClasses .= " has-bio";
-?>
+  ?>
   <div class="item record<?php echo $itemClasses ?>">
     <div class="item-img">
       <?php echo item_image('square_thumbnail', array('alt' => $itemTitle)); ?>
     </div>
+
     <div class="item-meta">
-      <h2><?php echo people_get_link_to_item($itemTitle, array('class'=>'permalink')); ?></h2>
+      <h2><?php echo mcjc_link_to_item($itemTitle, $item); ?></h2>
       <?php if (metadata('item', 'has files')): ?>
       <?php endif; ?>
-      <?php if ($description = metadata('item', array('Dublin Core', 'Description'), array('snippet'=>250))): ?> <div
-        class="item-description">
+
+      <?php if ($description = metadata('item', array('Dublin Core', 'Description'), array('snippet'=>250))): ?>
+      <div class="item-description">
         <?php echo $description; ?>
       </div>
-      <?php endif; ?><?php fire_plugin_hook('public_items_browse_each', array('view' => $this, 'item' =>$item)); ?>
-      <?php fire_plugin_hook('public_items_browse', [
-    'items' => $items,
-    'view' => $this,
-]); ?>
-    </div>
-    <!-- end class="item-meta" -->
-  </div><!-- end class="item entry" -->
-</div>
-<?php endforeach; ?>
+      <?php endif; ?>
+
+      <?php fire_plugin_hook('public_items_browse_each', array('view' => $this, 'item' =>$item)); ?>
+
+
+
+    </div><!-- end class="item-meta" -->
+    <div class="person-name"><?php echo mcjc_link_to_item($itemTitle); ?></div>
+    <!-- end class="item entry" -->
+  </div>
+
+  <?php endforeach; ?>
 </div>
 <div <?php echo pagination_links(); ?>
   <?php fire_plugin_hook('public_items_browse', array('items'=> $items, 'view' => $this)); ?> <?php echo foot(); ?>
