f501a70f53284d24c19e4b185d96f8f60c52ac26
Tim Stallmann <tim@rad.cat>
2020-04-10 12:35:08 -0400

    Fix misc typos/errors

--- htdocs/themes/berlin_mcjc/people/index/browse.php
+++ htdocs/themes/berlin_mcjc/people/index/browse.php
@@ -1,16 +1,15 @@
 <?php
 
-
-$browseByPerson = TRUE;
+$browseByPerson = true;
 $pageTitle = __('Meet our Neighbors');
 $curLetter = $vars['cur_letter'];
 $validLetters = $vars['letters'];
 
-echo head(array('title'=>$pageTitle,'bodyclass' => 'people browse'));
+echo head(['title' => $pageTitle, 'bodyclass' => 'people browse']);
 ?>
 <div class="top-header">
 
-  <h1><?php echo $pageTitle;?></h1>
+  <h1><?php echo $pageTitle; ?></h1>
   <p>
     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer suscipit diam a nulla tempus rhoncus. Aliquam erat
     volutpat.
@@ -20,17 +19,19 @@ echo head(array('title'=>$pageTitle,'bodyclass' => 'people browse'));
 <div class="search-by-lastname">
   <span><?php echo __('SEARCH BY LAST NAME'); ?></span>
   <ul>
-    <li<?php echo (!$curLetter ? ' class="active"' : '');?>><a
-        href="<?php echo $this->url(array(), 'peopleDefault')?>">All</a></li>
+    <li<?php echo !$curLetter ? ' class="active"' : ''; ?>><a
+        href="<?php echo $this->url([], 'peopleDefault'); ?>">All</a></li>
       <?php foreach (range('A', 'Z') as $letter): ?>
       <?php if (in_array($letter, $validLetters)): ?>
-      <li<?php echo ($curLetter == $letter ? ' class="active"' : '');?>>
+      <li<?php echo $curLetter == $letter ? ' class="active"' : ''; ?>>
         <a
-          href="<?php echo $this->url(array(), 'peopleDefault', array('firstLetter' => $letter)) ?>"><?php echo $letter ?></a>
+          href="<?php echo $this->url([], 'peopleDefault', [
+              'firstLetter' => $letter,
+          ]); ?>"><?php echo $letter; ?></a>
         </li>
         <?php else: ?>
         <li class="disabled">
-          <?php echo $letter ?>
+          <?php echo $letter; ?>
         </li>
         <?php endif; ?>
         <?php endforeach; ?>
@@ -41,14 +42,18 @@ echo head(array('title'=>$pageTitle,'bodyclass' => 'people browse'));
 
 
   <?php foreach (loop('items') as $item): ?> <?php
-    $itemTitle = metadata('item', 'display_title');
-    $itemClasses = "";
-    if (metadata('item', 'has files')) $itemClasses = " has-picture";
-    if (metadata('item', array('Dublin Core', 'Description'))) $itemClasses .= " has-bio";
-  ?>
-  <div class="item record<?php echo $itemClasses ?>">
+ $itemTitle = metadata('item', 'display_title');
+ $itemClasses = "";
+ if (metadata('item', 'has files')) {
+     $itemClasses = " has-picture";
+ }
+ if (metadata('item', ['Dublin Core', 'Description'])) {
+     $itemClasses .= " has-bio";
+ }
+ ?>
+  <div class="item record<?php echo $itemClasses; ?>">
     <div class="item-img">
-      <?php echo item_image('square_thumbnail', array('alt' => $itemTitle)); ?>
+      <?php echo item_image('square_thumbnail', ['alt' => $itemTitle]); ?>
     </div>
 
     <div class="item-meta">
@@ -56,13 +61,22 @@ echo head(array('title'=>$pageTitle,'bodyclass' => 'people browse'));
       <?php if (metadata('item', 'has files')): ?>
       <?php endif; ?>
 
-      <?php if ($description = metadata('item', array('Dublin Core', 'Description'), array('snippet'=>250))): ?>
+      <?php if (
+          $description = metadata(
+              'item',
+              ['Dublin Core', 'Description'],
+              ['snippet' => 250]
+          )
+      ): ?>
       <div class="item-description">
         <?php echo $description; ?>
       </div>
       <?php endif; ?>
 
-      <?php fire_plugin_hook('public_items_browse_each', array('view' => $this, 'item' =>$item)); ?>
+      <?php fire_plugin_hook('public_items_browse_each', [
+          'view' => $this,
+          'item' => $item,
+      ]); ?>
 
 
 
@@ -74,4 +88,7 @@ echo head(array('title'=>$pageTitle,'bodyclass' => 'people browse'));
   <?php endforeach; ?>
 </div>
 <div <?php echo pagination_links(); ?>
-  <?php fire_plugin_hook('public_items_browse', array('items'=> $items, 'view' => $this)); ?> <?php echo foot(); ?>
+<?php fire_plugin_hook('public_items_browse', [
+    'items' => $items,
+    'view' => $this,
+]); ?> <?php echo foot(); ?>
--- htdocs/themes/berlin_mcjc/search/index.php
+++ htdocs/themes/berlin_mcjc/search/index.php
@@ -8,21 +8,19 @@ $searchRecordTypes = get_search_record_types();
 <?php if ($total_results): ?>
 
 <div id="search-results">
-  <!-- <div class="search-results-headers">
-    
-      <span class="table-header--1"><?php echo __('Record');?></span>
-      <span class="table-header--2"><?php echo __('Media');?></span>
-      <span class="table-header--3"><?php echo __('Type');?></span>
-    
-    </div> -->
   <div class="search-results-body">
-    <?php $filter = new Zend_Filter_Word_CamelCaseToDash; ?>
+    <?php $filter = new Zend_Filter_Word_CamelCaseToDash(); ?>
     <?php foreach (loop('search_texts') as $searchText): ?>
-    <?php $record = get_record_by_id($searchText['record_type'], $searchText['record_id']); ?>
+    <?php $record = get_record_by_id(
+        $searchText['record_type'],
+        $searchText['record_id']
+    ); ?>
     <?php $recordType = $searchText['record_type']; ?>
     <?php set_current_record($recordType, $record); ?>
 
-    <div class="search-results-rows" class="<?php echo strtolower($filter->filter($recordType)); ?>">
+    <div class="search-results-rows" class="<?php echo strtolower(
+        $filter->filter($recordType)
+    ); ?>">
       <span class="results-row-img">
         <?php if ($recordImage = record_image($recordType)): ?>
         <?php echo mcjc_files_for_item(strtolower($recordType)); ?>
@@ -34,7 +32,12 @@ $searchRecordTypes = get_search_record_types();
         </span>
         <span>
           <h2 class="search-item__title"><a
-              href="<?php echo record_url($record, 'show'); ?>"><?php echo $searchText['title'] ? $searchText['title'] : '[Unknown]'; ?></a>
+              href="<?php echo record_url(
+                  $record,
+                  'show'
+              ); ?>"><?php echo $searchText['title']
+    ? $searchText['title']
+    : '[Unknown]'; ?></a>
           </h2>
         </span>
 
@@ -46,7 +49,7 @@ $searchRecordTypes = get_search_record_types();
 <?php echo pagination_links(); ?>
 <?php else: ?>
 <div id="no-results">
-  <p><?php echo __('Your query returned no results.');?></p>
+  <p><?php echo __('Your query returned no results.'); ?></p>
 </div>
 <?php endif; ?>
 <?php echo foot(); ?>
