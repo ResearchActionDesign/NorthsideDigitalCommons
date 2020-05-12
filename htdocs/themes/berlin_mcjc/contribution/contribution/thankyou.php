db7e720ede0577d244971a2f87cb293c97ba88df
osbaldo aguilar <osbaldoaguilar@osbaldos-MacBook-Air.local>
2020-05-12 17:18:49 -0400

    1stCommit

--- htdocs/themes/berlin_mcjc/contribution/contribution/thankyou.php
+++ htdocs/themes/berlin_mcjc/contribution/contribution/thankyou.php
@@ -1,22 +1,39 @@
 <?php echo head(['bodyclass' => 'contribution thank-you']); ?>
+
 <div class="title">
     <h1><?php echo __("Add Your Story"); ?></h1>
 </div>
-<div id="primary">
-    <h2><?php echo __("Thank you for contributing!"); ?></h2>
-    <p><?php echo __(
-        "Your contribution will show up in the archive once an administrator approves it."
+<div class='header-background-container add-story-image'>
+    <div class="header-background-container-content">
+        <!-- <h2 class='image-title'>  </h2> -->
+        <h2 class='image-title'><?php echo __("Tell your Story"); ?></h2>
+        <div class='image-text'>
+            <p>Loerum Ipsoms dolor sit amet, consectetur</p>
+            <p>adipiscing elit. Integer suscitpit diam a nulla</p>
+            <p>tempus rhoncus. Aliquam erat volutpat</p>
+        </div>
+        
+    </div>
+</div>
+<div id="primary" class='contribute-content'>
+    <h2 class='contribute-title'><?php echo __(
+        "Thank you for contributing!"
+    ); ?></h2>
+    <p class='contribute-paragraph'><?php echo __(
+        "Your contribution will show up in the archive once an administrator approves it. Meanwhile, feel free to make another contribution or browse the archive"
     ); ?>
-	</p>
-    <a class="button" href="<?php echo contribution_contribute_url(); ?>"><?php echo __(
-    'Add another story'
+    </p>
+    <div class='contribute-button-wrapper'>
+            <a class="button contribute" href="<?php echo contribution_contribute_url(); ?>"><?php echo __(
+    'Make another contribution'
 ); ?></a>
-    <a class='button' href="<?php echo url(
-        [],
-        'peopleDefault'
-    ); ?>"><?php echo __('View more stories'); ?></a>
+            <a class='button browseTopics' href="<?php echo url(
+                [],
+                'peopleDefault'
+            ); ?>"><?php echo __('Browse all Topics'); ?></a>
+    </div>
 	<?php if (get_option('contribution_open') && !current_user()): ?>
-	<p><?php echo __(
+	<p class='contribute-paragraph'><?php echo __(
      "If you would like to interact with the site further, you can use an account that is ready for you. Visit %s, and request a new password for the email you used",
      "<a href='" .
          url('users/forgot-password') .
