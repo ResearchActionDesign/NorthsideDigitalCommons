b7c832e7d1f16bbf0849ce7433d84c10d0cd573d
Kevin Arevalo <arevalok91@gmail.com>
2020-05-27 18:47:34 -0400

    Styled collection page and added placeholsers for related items and added media quieres

--- htdocs/themes/berlin_mcjc/collections/show.php
+++ htdocs/themes/berlin_mcjc/collections/show.php
@@ -6,32 +6,49 @@ $collectionTitle = metadata('collection', 'display_title'); ?>
     'bodyclass' => 'collections show',
 ]); ?>
 
-<h1><?php echo $collectionTitle; ?></h1>
-
-<div id="item-description">
-  <?php echo metadata('collection', ['Dublin Core', 'Description']); ?>
+<div class="collection-top-header">
+  <h1><?php echo $collectionTitle; ?></h1>
+  <div class="header-description">
+    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quod libero nulla quia autem? Sapiente
+      aliquid voluptatibus veritatis. Consequatur, voluptatem! Facilis, accusantium doloremque. Eaque, ipsum corporis?
+      Animi dolorem numquam ullam!</p>
+    <div class="collection-header-img">PLACEHOLDER</div>
+  </div>
 </div>
 
-<div id="collection-items">
-  <h2><?php echo link_to_items_browse(
-      __('Items in the %s Collection', $collectionTitle),
-      ['collection' => metadata('collection', 'id')]
-  ); ?></h2>
+<div class="collection-main-content">
+  <div class="collection-description">
+    <?php echo metadata('collection', ['Dublin Core', 'Description']); ?>
+  </div>
+  <div class=" related-items">PlaceHolder</div>
+</div>
+
+
+<div class="collection-grid">
+
   <?php if (metadata('collection', 'total_items') > 0): ?>
   <?php foreach (loop('items') as $item): ?>
   <?php $itemTitle = metadata('item', 'display_title'); ?>
-  <div class="item entry">
-    <h3><?php echo mcjc_link_to_item($itemTitle, $item); ?></h3>
 
-    <?php if ($collectionImage = record_image('collection')): ?>
-    <div class="item-img">
-      <div class="item-images"><?php echo mcjc_files_for_item(
+  <div class="collection item">
+    <div class="collection-item-img">
+      <?php if ($collectionImage = record_image('collection')): ?>
+      <?php echo mcjc_files_for_item(
           'item',
           [],
           ['class' => 'item-file'],
           $item
-      ); ?></div>
+      ); ?>
     </div>
+    <div class="collection item-meta">
+      <div class="item-description">
+        <?php echo $description; ?>
+      </div>
+    </div><!-- end class="item-meta" -->
+    <div class="item-title"><?php echo mcjc_link_to_item(
+        $itemTitle,
+        $item
+    ); ?></div>
     <?php endif; ?>
 
     <?php if (
@@ -41,9 +58,7 @@ $collectionTitle = metadata('collection', 'display_title'); ?>
             ['snippet' => 250]
         )
     ): ?>
-    <div class="item-description">
-      <?php echo $description; ?>
-    </div>
+
     <?php endif; ?>
   </div>
   <?php endforeach; ?>
@@ -52,7 +67,8 @@ $collectionTitle = metadata('collection', 'display_title'); ?>
       'There are currently no items within this collection.'
   ); ?></p>
   <?php endif; ?>
-</div><!-- end collection-items -->
+</div>
+<!-- end collection-grid -->
 
 <?php fire_plugin_hook('public_collections_show', [
     'view' => $this,
