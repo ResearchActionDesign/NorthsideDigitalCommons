96427fdb29e9e165f39c81a8e1cd7562f2a6bda4
Kevin Arevalo <arevalok91@gmail.com>
2020-07-08 13:49:32 -0400

    Fixed issues, typography, homepage, item grids

--- htdocs/themes/berlin_mcjc/contribution/contribution/terms.php
+++ htdocs/themes/berlin_mcjc/contribution/contribution/terms.php
@@ -1,24 +1,23 @@
 <?php
 $head = ['title' => __('Add Your Story'), 'bodyclass' => 'contribution terms'];
 echo head($head);
-$imageTitle = __("Tell Your Story");
+$imageTitle = __('Tell Your Story');
 ?>
 <div class="title">
-    <div class='header-background-container add-story-image'>
-        <div class="header-background-container-content">
-            <h1 class='image-title'> <?php echo $imageTitle; ?> </h1>
-            <div class='image-text'>
-                <p>Loerum Ipsoms dolor sit amet, consectetur</p>
-                <p>adipiscing elit. Integer suscitpit diam a nulla</p>
-                <p>tempus rhoncus. Aliquam erat volutpat</p>
-            </div>
-        </div>
+  <div class='header-background-container add-story-image'>
+    <div class="header-background-container-content">
+      <h1 class='image-title'> <?php echo $imageTitle; ?> </h1>
+      <p class="image-text"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, quo saepe non magnam
+        cum, molestiae
+        incidunt voluptatum, hic nisi dolor fuga? Mollitia magnam velit aliquid voluptatem saepe sit alias laboriosam.
+      </p>
     </div>
+  </div>
 </div>
 <div id="primary">
-    <div class='contribution-content-wrapper'>
+  <div class='contribution-content-wrapper'>
     <h2><?php echo __('Terms of Service'); ?></h2>
-<?php echo get_option('contribution_consent_text'); ?>
-    </div>
+    <?php echo get_option('contribution_consent_text'); ?>
+  </div>
 </div>
 <?php echo foot(); ?>
