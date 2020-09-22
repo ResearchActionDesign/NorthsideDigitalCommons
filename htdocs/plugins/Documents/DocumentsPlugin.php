7fa932219659920badc8fd50f044555152f90bcf
Tim Stallmann <tim@rad.cat>
2020-09-21 20:09:54 -0400

    Implement /documents urls

---
+++ htdocs/plugins/Documents/DocumentsPlugin.php
@@ -0,0 +1,41 @@
+<?php
+
+class DocumentsPlugin extends Omeka_Plugin_AbstractPlugin
+{
+
+  protected $_hooks = array(
+    'define_routes',
+  );
+
+  /**
+   * People define_routes hook
+   * Defines public-only routes for browse all stories and view a single oral history.
+   */
+  public function hookDefineRoutes($args)
+  {
+    $router = $args['router'];
+
+    $router->addRoute('imagesBrowse',
+      new Zend_Controller_Router_Route('documents',
+        array(
+          'module'     => 'documents',
+          'controller' => 'index',
+          'action'     => 'browse',
+        )
+      )
+    );
+
+    $router->addRoute('imagesShow',
+      new Zend_Controller_Router_Route('documents/:permalink',
+        array(
+          'module'     => 'documents',
+          'controller' => 'items',
+          'action'     => 'show'
+        ),
+        array(
+          'permalink' => '[a-zA-z0-9][a-zA-Z0-9\-]*'
+        )
+      )
+    );
+  }
+}
---
+++ htdocs/themes/berlin_mcjc/documents/index/browse.php
@@ -0,0 +1,48 @@
+<?php
+
+$pageTitle = __('Browse Items');
+echo head(['title' => $pageTitle, 'bodyclass' => 'items browse']);
+?>
+
+<h1><?php echo $pageTitle; ?> <?php echo __(
+     '(%s total)',
+     $total_results
+ ); ?></h1>
+
+<nav class="items-nav navigation secondary-nav">
+  <?php echo public_nav_items(); ?>
+</nav>
+
+<?php echo item_search_filters(); ?>
+
+<?php if ($total_results > 0):
+
+    $sortLinks[__('Title')] = 'Dublin Core,Title';
+    $sortLinks[__('Creator')] = 'Dublin Core,Creator';
+    $sortLinks[__('Date Added')] = 'added';
+    ?>
+<div id="sort-links">
+  <span class="sort-label"><?php echo __(
+      'Sort by: '
+  ); ?></span><?php echo browse_sort_links($sortLinks); ?>
+</div>
+
+<?php
+endif; ?>
+
+<div class="grid-container">
+    <div class="grid-items">
+      <?php foreach (loop('items') as $item): ?>
+        <?php echo common('grid-item', ['item' => $item]); ?>
+      <?php endforeach; ?>
+    </div>
+</div>
+
+<?php echo pagination_links(); ?>
+
+<?php fire_plugin_hook('public_items_browse', [
+    'items' => $items,
+    'view' => $this,
+]); ?>
+
+<?php echo foot(); ?>
