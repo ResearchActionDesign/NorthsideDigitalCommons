dad64abbcc63659cbced8c52940e2e6293e70d2c
Tim Stallmann <tim@rad.cat>
2021-01-21 13:17:53 -0500

    Update share language

--- htdocs/themes/berlin_mcjc/common/share_icons.php
+++ htdocs/themes/berlin_mcjc/common/share_icons.php
@@ -1,5 +1,9 @@
 <div class="share-icons">
-  <button class="share-button" data-sharer="twitter" data-title="<?php echo $title; ?>" data-url="<?php echo $url; ?>">
+  <button class="share-button"
+          data-sharer="twitter"
+          data-title="Look what I found on From the Rock Wall, an interactive site featuring oral histories of Black Chapel Hill/Carrboro!"
+          data-via="MCJCenter"
+          data-url="<?php echo $url; ?>">
     <i class="fa fa-twitter"></i>
     <span class="sr-only">Share on Twitter</span>
   </button>
@@ -7,7 +11,7 @@
     <i class="fa fa-facebook"></i>
     <span class="sr-only">Share on Facebook</span>
   </button>
-  <button class="share-button" data-sharer="email" data-title="<?php echo $title; ?>" data-to="" data-url="<?php echo $url; ?>">
+  <button class="share-button" data-sharer="email" data-subject="Look what I found on From the Rock Wall!" data-to="" data-url="<?php echo $url; ?>">
     <i class="fa fa-envelope"></i>
     <span class="sr-only">Share via email</span>
   </button>
