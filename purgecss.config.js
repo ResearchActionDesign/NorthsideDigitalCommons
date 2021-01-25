1bb1f8ff67834e9c92a6afe42bfd0ae66224a8fd
Tim Stallmann <tim@rad.cat>
2021-01-25 15:38:58 -0500

    Add purgecss

---
+++ purgecss.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "htdocs/themes/berlin_mcjc/**/*.php",
+    "htdocs/themes/berlin_mcjc/**/*.js",
+  ],
+  css: ["htdocs/themes/berlin_mcjc/css/style.css"],
+  output: "htdocs/themes/berlin_mcjc/css/style.css",
+  safelist: {
+    greedy: [
+      /webp/,
+      /hero-image/,
+      /playing/,
+      /mejs/,
+      /lity/,
+      /slick/,
+      /admin-bar/,
+    ],
+  },
+};
