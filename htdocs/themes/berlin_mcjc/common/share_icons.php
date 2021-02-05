46ba55dc0a15350aa0314e57674e8708b9e3bf5b
Tim Stallmann <tim@rad.cat>
2021-02-05 13:59:24 -0500

    Add new banner to homepage, other updates

--- htdocs/themes/berlin_mcjc/common/share_icons.php
+++ htdocs/themes/berlin_mcjc/common/share_icons.php
@@ -1,19 +1,17 @@
 <div class="share-icons">
-    Click an icon to share on Twitter
-  <button class="share-button"
+    Share on
+    <button class="share-button"
           data-sharer="twitter"
           data-title="Look what I found on From the Rock Wall, an interactive site featuring oral histories of Black Chapel Hill/Carrboro!"
           data-via="MCJCenter"
           data-url="<?php echo $url; ?>">
-    <i class="fa fa-twitter"></i>
-    <span class="sr-only">Share on Twitter</span>
-  </button>, Facebook
+       Twitter <i class="fa fa-twitter"></i>
+  </button>,
   <button class="share-button" data-sharer="facebook" data-url="<?php echo $url; ?>">
-    <i class="fa fa-facebook"></i>
-    <span class="sr-only">Share on Facebook</span>
-  </button>, or by email
+      Facebook
+      <i class="fa fa-facebook"></i>
+  </button>, or
   <button class="share-button" data-sharer="email" data-subject="Look what I found on From the Rock Wall!" data-to="" data-url="<?php echo $url; ?>">
-    <i class="fa fa-envelope"></i>
-    <span class="sr-only">Share via email</span>
+      by email<i class="fa fa-envelope"></i>
   </button>.
 </div>
