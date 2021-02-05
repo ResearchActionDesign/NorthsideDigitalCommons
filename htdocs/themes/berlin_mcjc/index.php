0c371c63d65bc10818ab2e5299eab9ca2e3de77d
Tim Stallmann <tim@rad.cat>
2021-02-05 18:41:45 -0500

    Add slider for items with multiple images

--- htdocs/themes/berlin_mcjc/index.php
+++ htdocs/themes/berlin_mcjc/index.php
@@ -140,31 +140,10 @@
                 </div>
             <?php endforeach; ?>
           </div>
-          <ul class="slider-controls" id="have-you-heard__controls" aria-label="Carousel Navigation" tabindex="0">
-              <li class="prev" aria-controls="customize" tabindex="-1" data-controls="prev" aria-label="Previous slide">
-                  <i class="fa fa-arrow-circle-left"></i>
-              </li>
-              <li class="next" aria-controls="customize" tabindex="-1" data-controls="next" aria-label="Next slide">
-                  <i class="fa fa-arrow-circle-right"></i>
-              </li>
-          </ul>
+        <?php echo common('slider-markup', [
+            'class' => 'have-you-heard__container',
+        ]); ?>
       <?php endif; ?>
   </div>
 </div>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.3/tiny-slider.css">
-<script src="https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.2/min/tiny-slider.js"></script>
-<script type="module">
-    var slider = tns({
-        container: '.have-you-heard__container',
-        items: 1,
-        gutter: 16,
-        controlsContainer: '#have-you-heard__controls',
-        nav: false,
-        responsive: {
-            "768": {
-                edgePadding: 140,
-            }
-        }
-    });
-</script>
 <?php echo foot(['bodyid' => 'home']); ?>
