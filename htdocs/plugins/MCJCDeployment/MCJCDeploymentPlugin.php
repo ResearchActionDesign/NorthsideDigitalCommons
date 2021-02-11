aa2d263ff6c6deef14850209b234beb705b8ea9b
Tim Stallmann <tim@rad.cat>
2021-02-11 13:23:06 -0500

    Tweak simplepages div

--- htdocs/plugins/MCJCDeployment/MCJCDeploymentPlugin.php
+++ htdocs/plugins/MCJCDeployment/MCJCDeploymentPlugin.php
@@ -620,7 +620,7 @@ class MCJCDeploymentPlugin extends Omeka_Plugin_AbstractPlugin
       return '';
     }
     return <<<EOL
-        <div class="typeform-widget" data-url="{$url}" style="width: 100%; height: {$height}px;"></div>
+        <div class="typeform-widget" data-url="{$url}" style="width: 100%; height: {$height}px; clear: both;"></div>
         <script> (function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm", b="https://embed.typeform.com/"; if(!gi.call(d,id)) { js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })() </script>
   EOL;
   }
