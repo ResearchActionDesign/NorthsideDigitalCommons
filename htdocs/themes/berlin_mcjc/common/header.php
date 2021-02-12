7a11179470d5600f87814ae4e2a4557d166b62ce
Tim Stallmann <tim@rad.cat>
2021-02-12 12:51:40 -0500

    Tweak tag manager load

--- htdocs/themes/berlin_mcjc/common/header.php
+++ htdocs/themes/berlin_mcjc/common/header.php
@@ -22,6 +22,16 @@ $titleParts[] = option('site_title');
 $title = implode(' | ', $titleParts);
 ?>
 <head>
+  <?php if ($google_id = get_theme_option('google_analytics_id')): ?>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','<?php echo $google_id; ?>');</script>
+    <!-- End Google Tag Manager -->
+  <?php endif; ?>
+
     <?php
 /** Load google fonts. @see https://csswizardry.com/2020/05/the-fastest-google-fonts/ */
 ?>
@@ -94,19 +104,14 @@ $title = implode(' | ', $titleParts);
         <link rel="stylesheet"
               href="/themes/berlin_mcjc/css/noscript.css?v=<?php echo $commit_hash; ?>" />
     </noscript>
-    <?php if ($google_id = get_theme_option('google_analytics_id')): ?>
-        <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo $google_id; ?>"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', '<?php echo $google_id; ?>');
-    </script>
-    <?php endif; ?>
 </head>
 <?php echo body_tag(['id' => $bodyid ?? '', 'class' => $bodyclass ?? '']); ?>
+<?php if ($google_id): ?>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?php echo $google_id; ?>"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+<?php endif; ?>
 <div class="page-header-background-container">
     <div class="page-header-container">
 <a href="#content" id="skipnav"><?php echo __('Skip to main content'); ?></a>
