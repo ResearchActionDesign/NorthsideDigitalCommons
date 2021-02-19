65d898ae6224a4eb31e5abe52822f153ed4440b9
Tim Stallmann <tim@rad.cat>
2021-02-19 15:14:18 -0500

    Tweak google analytics

--- htdocs/themes/berlin_mcjc/common/header.php
+++ htdocs/themes/berlin_mcjc/common/header.php
@@ -23,13 +23,16 @@ $title = implode(' | ', $titleParts);
 ?>
 <head>
   <?php if ($google_id = get_theme_option('google_analytics_id')): ?>
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','<?php echo $google_id; ?>');</script>
-    <!-- End Google Tag Manager -->
+      <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo $google_id; ?>"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', '<?php echo $google_id; ?>');
+</script>
+<!-- End Google Tag Manager -->
   <?php endif; ?>
 
     <?php
