40eba6a05769a40cb799c21413dd494ed2418e18
Tim Stallmann <tim@rad.cat>
2022-11-28 10:59:48 -0500

    Upgrade rollbar

--- htdocs/plugins/MCJCDeployment/vendor/composer/installed.php
+++ htdocs/plugins/MCJCDeployment/vendor/composer/installed.php
@@ -1,58 +1,56 @@
-<?php return array (
-  'root' => 
-  array (
-    'pretty_version' => 'dev-develop',
-    'version' => 'dev-develop',
-    'aliases' => 
-    array (
+<?php return array(
+    'root' => array(
+        'name' => 'timstallmann/mcjc-deployment',
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'reference' => '2c3ce51a15c8fa1842c4b8f997875d18b8b07167',
+        'type' => 'plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
     ),
-    'reference' => '673b103c9812e137b6b7467e010bf733497296fe',
-    'name' => 'timstallmann/mcjc-deployment',
-  ),
-  'versions' => 
-  array (
-    'monolog/monolog' => 
-    array (
-      'pretty_version' => '2.2.0',
-      'version' => '2.2.0.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '1cb1cde8e8dd0f70cc0fe51354a59acad9302084',
+    'versions' => array(
+        'monolog/monolog' => array(
+            'pretty_version' => '2.8.0',
+            'version' => '2.8.0.0',
+            'reference' => '720488632c590286b88b80e62aa3d3d551ad4a50',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../monolog/monolog',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'psr/log' => array(
+            'pretty_version' => '2.0.0',
+            'version' => '2.0.0.0',
+            'reference' => 'ef29f6d262798707a9edd554e2b82517ef3a9376',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../psr/log',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'psr/log-implementation' => array(
+            'dev_requirement' => false,
+            'provided' => array(
+                0 => '1.0.0 || 2.0.0 || 3.0.0',
+            ),
+        ),
+        'rollbar/rollbar' => array(
+            'pretty_version' => 'v3.1.4',
+            'version' => '3.1.4.0',
+            'reference' => '169cee9ef78139558d6a3e5f680afb5853f4228e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../rollbar/rollbar',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'timstallmann/mcjc-deployment' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => '2c3ce51a15c8fa1842c4b8f997875d18b8b07167',
+            'type' => 'plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
     ),
-    'psr/log' => 
-    array (
-      'pretty_version' => '1.1.3',
-      'version' => '1.1.3.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '0f73288fd15629204f9d42b7055f72dacbe811fc',
-    ),
-    'psr/log-implementation' => 
-    array (
-      'provided' => 
-      array (
-        0 => '1.0.0',
-      ),
-    ),
-    'rollbar/rollbar' => 
-    array (
-      'pretty_version' => 'v2.1.0',
-      'version' => '2.1.0.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'ff3db5739dd635740caed02ddad43e671b5a37e5',
-    ),
-    'timstallmann/mcjc-deployment' => 
-    array (
-      'pretty_version' => 'dev-develop',
-      'version' => 'dev-develop',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '673b103c9812e137b6b7467e010bf733497296fe',
-    ),
-  ),
 );
